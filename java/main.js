2da6dccd1808c9edf0b631093d6636d180272fda
ACE User <aceuser@e140-s05.lab.lagcc>
2017-06-12 21:17:39 -0400

    js

--- java/main.js
+++ java/main.js
@@ -1,1 +1,20 @@
-console.log('works');
+// console.log('works');
+var topNavList=document.querySelector('#topnavlist');
+var topNavToggle=document.querySelector('#topnavtoggle')
+var navCollapse=true;
+
+topNavToggle.addEventListener('click',function(){
+
+		// console.log('works');
+		if(navCollapse===true){
+			topNavList.classList.remove('navlistcollapse');
+			topNavToggle.innerHTML='<i class="fa fa-times" aria-hidden="true"></i>';
+			navCollapse=false;
+
+		}
+		else{
+			topNavList.classList.add('navlistcollapse');
+			topNavToggle.innerHTML='<i class="fa fa-bars" aria-hidden="true"></i>';
+			navCollapse=true;
+		}
+});
